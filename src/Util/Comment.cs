f6c50b9665eff232f72dfd1f5ffc6fcecfd71dc1
larrykirschner <larrykirschner@gmail.com>
2017-08-17 16:52:10 -0700

    install initial

---
+++ src/Util/Comment.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BeatThat
+{
+
+	/// <summary>
+	/// Adding comments to GameObjects in the Inspector.
+	/// </summary>
+	public class Comment : MonoBehaviour 
+	{
+		[Multiline(10)]
+		public string text;
+	}
+}
